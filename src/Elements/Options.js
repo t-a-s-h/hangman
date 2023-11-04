4462ee84158f8cc399bfa934ec989a3ecafcd5bd
Natasha Fray <natasha@Natashas-MacBook-Air.local>
2023-11-04 19:32:17 -0400

    got rid of API and other changes

--- src/Elements/Options.js
+++ src/Elements/Options.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Options = () => <div className>
+const Options = () => <div className={'options'}>
     <a href='/game'>You guess the word</a><br/>
     <a href='/auto'>The computer guesses the word</a></div>
 
